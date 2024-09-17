df0c59b127247189bd476c8db0069ff0b3aed9b3
TheDudeFromCI <thedudefromci@gmail.com>
2024-09-17 03:10:31 -0700

    Fixed splash screen init timing.
    Signed-off-by: TheDudeFromCI <thedudefromci@gmail.com>

--- src/ui/menu.rs
+++ src/ui/menu.rs
@@ -9,17 +9,28 @@ pub struct MainMenuPlugin;
 impl Plugin for MainMenuPlugin {
     fn build(&self, app_: &mut App) {
         app_.init_state::<MainMenuState>()
-            .add_plugins(super::splash::SplashPlugin);
+            .add_plugins(super::splash::SplashPlugin)
+            .add_systems(Startup, to_splash_screen);
     }
 }
 
 /// The main menu state machine.
 #[derive(Debug, Default, Clone, Copy, PartialEq, Eq, Hash, States)]
 pub enum MainMenuState {
-    /// The splash screen state. This is the default menu state used on startup.
+    /// The initial state. This state is active only for a single frame while
+    /// the application window is loading. This is the default menu state used
+    /// on startup.
     #[default]
+    Init,
+
+    /// The splash screen state.
     Splash,
 
     /// The project editor menu.
     Editor,
+}
+
+/// This system runs on startup to transition to the splash screen.
+fn to_splash_screen(mut state: ResMut<NextState<MainMenuState>>) {
+    state.set(MainMenuState::Splash);
 }
--- src/ui/splash.rs
+++ src/ui/splash.rs
@@ -14,7 +14,7 @@ const SPLASH_MAX_SIZE: f32 = 1024.0;
 pub struct SplashPlugin;
 impl Plugin for SplashPlugin {
     fn build(&self, app_: &mut App) {
-        app_.add_systems(Startup, build_splash)
+        app_.add_systems(OnEnter(MainMenuState::Splash), build_splash)
             .add_systems(OnExit(MainMenuState::Splash), dispose_splash)
             .add_systems(
                 Update,
@@ -27,12 +27,19 @@ impl Plugin for SplashPlugin {
 #[derive(Debug, Component)]
 struct SplashScreenRoot;
 
-/// This is a marker component that indicates the splash screen icon.
+/// This is a component that indicates the splash screen icon.
 #[derive(Debug, Component)]
-struct SplashIcon;
+struct SplashIcon {
+    /// The time the splash screen was initialized.
+    ///
+    /// Note: Elapsed seconds does not work, since the window usually takes a
+    /// few hundred milliseconds to initialize, so this offset is used to
+    /// account for that.
+    init_time: f32,
+}
 
 /// Builds the splash screen.
-fn build_splash(asset_server: Res<AssetServer>, mut commands: Commands) {
+fn build_splash(time: Res<Time>, asset_server: Res<AssetServer>, mut commands: Commands) {
     commands
         .spawn((
             SplashScreenRoot,
@@ -48,7 +55,9 @@ fn build_splash(asset_server: Res<AssetServer>, mut commands: Commands) {
         ))
         .with_children(|parent| {
             parent.spawn((
-                SplashIcon,
+                SplashIcon {
+                    init_time: time.elapsed_seconds(),
+                },
                 ImageBundle {
                     style: Style {
                         width: Val::Auto,
@@ -69,7 +78,7 @@ fn build_splash(asset_server: Res<AssetServer>, mut commands: Commands) {
 /// Updates the splash screen animation.
 fn update_splash(
     time: Res<Time>,
-    mut icon: Query<&mut UiImage, With<SplashIcon>>,
+    mut icon: Query<(&mut UiImage, &SplashIcon)>,
     mut next_state: ResMut<NextState<MainMenuState>>,
 ) {
     /// The time in seconds to wait before fading in the splash icon.
@@ -84,26 +93,26 @@ fn update_splash(
     /// The time in seconds to wait before transitioning to the main menu.
     const END_TIME: f32 = 1.0;
 
-    let seconds = time.elapsed_seconds();
+    for (mut image, icon) in icon.iter_mut() {
+        let seconds = time.elapsed_seconds() - icon.init_time;
 
-    let alpha = if seconds < INIT_TIME {
-        0.0
-    } else if seconds < INIT_TIME + FADE_TIME {
-        (seconds - INIT_TIME) / FADE_TIME
-    } else if seconds < INIT_TIME + FADE_TIME + HOLD_TIME {
-        1.0
-    } else if seconds < INIT_TIME + FADE_TIME + HOLD_TIME + FADE_TIME {
-        1.0 - (seconds - INIT_TIME - FADE_TIME - HOLD_TIME) / FADE_TIME
-    } else {
-        0.0
-    };
+        let alpha = if seconds < INIT_TIME {
+            0.0
+        } else if seconds < INIT_TIME + FADE_TIME {
+            (seconds - INIT_TIME) / FADE_TIME
+        } else if seconds < INIT_TIME + FADE_TIME + HOLD_TIME {
+            1.0
+        } else if seconds < INIT_TIME + FADE_TIME + HOLD_TIME + FADE_TIME {
+            1.0 - (seconds - INIT_TIME - FADE_TIME - HOLD_TIME) / FADE_TIME
+        } else {
+            0.0
+        };
 
-    for mut image in icon.iter_mut() {
         image.color = Color::srgba(1.0, 1.0, 1.0, alpha);
-    }
 
-    if seconds >= INIT_TIME + FADE_TIME + HOLD_TIME + FADE_TIME + END_TIME {
-        next_state.set(MainMenuState::Editor);
+        if seconds >= INIT_TIME + FADE_TIME + HOLD_TIME + FADE_TIME + END_TIME {
+            next_state.set(MainMenuState::Editor);
+        }
     }
 }
 
